ff21b4aa27c002b3e1bbd4522e4eb5030e0b8187
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2020-07-08 11:20:20 -0500

    deleted extra external crap

--- external-scripts/bin/config/gulpfile.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="node" />
-export declare const checkEnv: {
-    (): Promise<any>;
-    description: string;
-};
-export declare const regenerate: {
-    (): NodeJS.ReadWriteStream;
-    description: string;
-};
--- external-scripts/bin/src/pages/api/v2/info/all-extras.d.ts
+++
@@ -1,3 +0,0 @@
-import type { NextApiResponse, NextApiRequest } from 'next';
-export { config } from 'universe/backend/middleware';
-export default function (req: NextApiRequest, res: NextApiResponse): Promise<void>;
