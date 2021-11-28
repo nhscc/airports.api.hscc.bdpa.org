5c04c60611c01beb7405eb0dfd8bae311632ed6f
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2021-11-28 07:27:02 -0800

    build(ncurc): pin random-int

---
+++ .ncurc.js
@@ -0,0 +1,8 @@
+// * https://www.npmjs.com/package/npm-check-updates#configuration-files
+
+module.exports = {
+  reject: [
+    // ? Pin the working version of random-int
+    'random-int'
+  ]
+};
